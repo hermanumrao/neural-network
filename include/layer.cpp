dfc985932b0d712dd48dd6eccd8ae205d469af02
hermanumrao <hermanumrao@gmail.com>
2024-07-19 15:02:18 +0530

    added code for layers input, hidden, output

--- include/layer.cpp
+++ include/layer.cpp
@@ -5,5 +5,31 @@
 inp_layer::inp_layer(int size, double init_val) {
   for (int i = -0; i < size; i++)
     neurons.push_back(neuron(init_val));
-  std::cout << "new layer has been created" << std::endl;
+  std::cout << "new layer has been created (Input)" << std::endl;
+}
+
+hid_layer::hid_layer(int size, double init_val) {
+  for (int i = -0; i < size; i++)
+    neurons.push_back(neuron(init_val));
+  std::cout << "new layer has been created(hidden)" << std::endl;
+}
+
+void hid_layer::activate_layer(double (*func)(double)) {
+  for (auto neuron : neurons) {
+    neuron.set_act_val(func(neuron.get_value()));
+  }
+  std::cout << "Layer has been activated" << std::endl;
+}
+
+out_layer::out_layer(int size, double init_val) {
+  for (int i = -0; i < size; i++)
+    neurons.push_back(neuron(init_val));
+  std::cout << "new layer has been created(output)" << std::endl;
+}
+
+void out_layer::activate_layer(double (*func)(double)) {
+  for (auto neuron : neurons) {
+    neuron.set_act_val(func(neuron.get_value()));
+  }
+  std::cout << "Layer has been activated" << std::endl;
 }
--- include/layer.h
+++ include/layer.h
@@ -8,5 +8,22 @@ private:
 
 public:
   inp_layer(int size, double init_val);
+};
+
+class hid_layer : neuron {
+private:
+  std::vector<neuron> neurons;
+
+public:
+  hid_layer(int size, double init_val);
+  void activate_layer(double (*func)(double));
+};
+
+class out_layer : neuron {
+private:
+  std::vector<neuron> neurons;
+
+public:
+  out_layer(int size, double init_val);
   void activate_layer(double (*func)(double));
 };
